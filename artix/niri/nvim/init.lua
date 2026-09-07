326089da4ff4495a87bcec1c779f1a1096d4dcd2
Roman <157959459+Firomanse@users.noreply.github.com>
2026-09-07 17:17:43 +0300

    add new nvim init config

---
+++ artix/niri/nvim/init.lua
@@ -0,0 +1,9 @@
+require('globals')      -- global variables
+require('options')      -- nvim basic settings
+
+require('manager')      -- plugin manager
+
+require('keys/main')    -- keybindings for nvim
+require('keys/plugins') -- keybindings for plugins
+
+require('color')        -- color scheme
